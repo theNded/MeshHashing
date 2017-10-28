4113b945880be038aaf2221b0e8456b24f3a4c2d
wei <w.dong@pku.edu.cn>
2017-10-29 00:42:35 +0800

    primal dual function completed but not integrated

---
+++ src/mapping/update_convex.h
@@ -0,0 +1,16 @@
+//
+// Created by wei on 17-10-29.
+//
+
+#ifndef MESH_HASHING_UPDATE_CONVEX_H
+#define MESH_HASHING_UPDATE_CONVEX_H
+
+
+
+class update_convex {
+
+};
+
+
+
+#endif //MESH_HASHING_UPDATE_CONVEX_H
